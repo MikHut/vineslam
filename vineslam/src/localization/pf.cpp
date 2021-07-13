222cdfc0dbc019663cdd7362210f8ffe3ac3223b
white <white@robot.com>
2021-07-13 09:07:15 +0100

    Aveleda demo working version

--- vineslam/src/localization/pf.cpp
+++ vineslam/src/localization/pf.cpp
@@ -285,7 +285,7 @@ void PF::gps(const Pose& gps_pose, std::vector<float>& ws)
   {
     // - GPS [x, y] weight
     float w_gps;
-    //    float dist = particle.p_.distance(gps_pose);
+    // float dist = particle.p_.distance(gps_pose);
     float dist = particle.p_.distanceXY(gps_pose);
     w_gps = (normalizer_gps * static_cast<float>(std::exp(-1. / sigma_gps_ * dist)));
 
